80070c698b6967c4481316859f94b05ad437a748
Lancelot <lancelot@sunset.es>
2024-10-27 21:50:24 -0500

    ngrx added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
-    provideHttpClient() 
-  ]
+    provideHttpClient(), provideStore(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
